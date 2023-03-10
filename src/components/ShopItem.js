c407bc637a93f45364a36a8c8429eb73ac858d94
Ross Tarrant <rosstarrant3@gmail.com>
2023-03-10 06:57:36 +0000

    Add responsive design

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -38,15 +38,15 @@ export default function ShopItem({addToBasket}){
     }
 
     return(
-        <div className={"border-4 border-slate-800 shadow-lg rounded-lg flex gap-6 m-8 overflow-hidden font-jakarta"}>
-            <img alt={"pic"} src={shopItem.imgUrl} className={"w-1/2 border-slate-800 border-r-4"}/>
-            <div className={"w-1/2 flex flex-col justify-center gap-14 relative text-center"}>
-                <Link to={"/shop"} className={"absolute top-0 right-0 m-8"}>
+        <div className={"border-4 border-slate-800 shadow-lg rounded-lg flex flex-col items-center md:flex-row gap-6 m-8 overflow-hidden font-jakarta"}>
+            <img alt={"pic"} src={shopItem.imgUrl} className={"w-1/2 md:border-slate-800 border-r-4"}/>
+            <div className={"w-1/2 flex flex-col justify-center gap-14 relative text-center p-8"}>
+                <Link to={"/shop"} className={"flex flex-col items-end m-8"}>
                     <button className={"rounded-full bg-white-500 border-2 text-slate-800 border-slate-800 p-2 w-24 transition ease-out duration-300  hover:bg-slate-800 hover:text-white"}>Back</button>
                 </Link>
-                <h1 className={"text-6xl text-slate-700 font-bold"}>{shopItem.title}</h1>
-                <h2 className={"text-3xl"}>£{shopItem.price}</h2>
-                <p className={"text-2xl"}>{shopItem.description}</p>
+                <h1 className={"text-3xl md:text-4xl xl:text-6xl text-slate-700 font-bold"}>{shopItem.title}</h1>
+                <h2 className={"text-2xl md:text-3xl"}>£{shopItem.price}</h2>
+                <p className={"text-xl md:text-2xl"}>{shopItem.description}</p>
                 <div className={"flex justify-evenly items-center"}>
                     <div className={"flex gap-4 items-center"}>
                         <button onClick={onClickDecreaseQuantity} className={"text-4xl"}>-</button>
